fae54268222303503ef9a2758ac04d0bc1267eaa
julliabirsan <julliabirsan@yahoo.com>
2026-04-23 20:42:54 +0300

    pagination

--- src/main/java/com/link/OnlineShop/database/ProductDao.java
+++ src/main/java/com/link/OnlineShop/database/ProductDao.java
@@ -3,12 +3,14 @@ package com.link.OnlineShop.database;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductDao extends CrudRepository<Product, Integer> {
+public interface ProductDao extends JpaRepository<Product, Integer> {
 
+    //Page<Product> findAll(Pageable pageable);
 }
--- src/main/java/com/link/OnlineShop/service/ProductService.java
+++ src/main/java/com/link/OnlineShop/service/ProductService.java
@@ -3,6 +3,8 @@ package com.link.OnlineShop.service;
 import com.link.OnlineShop.database.Product;
 import com.link.OnlineShop.database.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,5 +16,9 @@ public class ProductService {
 
     public List<Product> findAllProducts(){
         return (List<Product>) productDao.findAll();
+    }
+
+    public Page<Product> getProductsPage(int page, int size){
+        return productDao.findAll(PageRequest.of(page, size));
     }
 }
